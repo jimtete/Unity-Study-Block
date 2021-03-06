826b7625388b77a9b5653b4a95d86bf0834ef7b8
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-03-06 10:00:31 +0200

    added prototype 2

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 14;
+    private int spawnPosZ = 25;
+    private int startDelay = 1;
+    private int spawnInterval = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        //It spawns a random animal in a random position.
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX + 1), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
