12ee3002e51af297d869faba6ed6bedde57004ca
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-03-11 18:56:47 +0200

    added prototype 4

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    private float powerUpStrength = 7.5f;
+    public float speed = 5.0f;
+    public bool pickedPowerUp = false;
+    public GameObject powerupIndicator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            powerupIndicator.gameObject.SetActive(true);
+            pickedPowerUp = true;
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        pickedPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && pickedPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0,-0.3f,0);
+
+        if (transform.position.y < -10) Debug.Log("Game over");
+    }
+}
---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerup;
+    private float spawnRange=8f;
+    public int enemyCount;
+    public int waveNumber = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        SpawnEnemyWave(waveNumber);
+        
+        
+    }
+
+    private Vector3 GenerateSpawnPoint()
+    {
+        float x = Random.Range(-spawnRange, spawnRange);
+        float z = Random.Range(-spawnRange, spawnRange);
+        return new Vector3(x, 0.15f, z);
+    }
+
+    void SpawnEnemyWave(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPoint(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(++waveNumber);
+            Instantiate(powerup, GenerateSpawnPoint(), powerup.transform.rotation);
+        }
+    }
+}
